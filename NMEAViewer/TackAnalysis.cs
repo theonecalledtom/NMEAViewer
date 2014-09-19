9e0de55930b826361261b979817a4a56ec761b0f
theonecalledtom <theonecalledtom@gmail.com>
2014-09-18 19:57:21 -0700

    Rename Util -> AngleUtil

--- NMEAViewer/TackAnalysis.cs
+++ NMEAViewer/TackAnalysis.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NMEAViewer
 {
-    class Util
+    class AngleUtil
     {
         public static void Swap(ref double rA, ref double rB)
         {
@@ -213,6 +213,16 @@ namespace NMEAViewer
             if (m_Data.GetDataCount() <= 0)
                 return null;
 
+            if (iStartSearch >= m_Data.GetDataCount())
+            {
+                return null;
+            }
+
+            if (iEndSearch >= m_Data.GetDataCount())
+            {
+                return null;
+            }
+
             List<int> tackPoints = new List<int>();
             int iToSearch = iEndSearch - 1;
             for (int i = iStartSearch; i < iToSearch - 1; i++)
@@ -230,12 +240,12 @@ namespace NMEAViewer
                 {
                     //Change in direction!
                     int iStart = System.Math.Max(0, i - 20);
-                    double fAngleAveragePre = Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.AWA, iStart, i);
-                    double fAvPrePoint = Util.ContainAngleMinus180To180(fAngleAveragePre);
+                    double fAngleAveragePre = AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.AWA, iStart, i);
+                    double fAvPrePoint = AngleUtil.ContainAngleMinus180To180(fAngleAveragePre);
 
                     int iTail = System.Math.Min(i + 20, iToSearch);
-                    double fAngleAveragePost = Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.AWA, i, iTail);
-                    double fAvPostPoint = Util.ContainAngleMinus180To180(fAngleAveragePost);
+                    double fAngleAveragePost = AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.AWA, i, iTail);
+                    double fAvPostPoint = AngleUtil.ContainAngleMinus180To180(fAngleAveragePost);
 
                     //Let's check we don't revert shortly
                     //TODO: Search better....
@@ -256,7 +266,7 @@ namespace NMEAViewer
         //We do this a lot so this reduces text...
         double AvAWAInRange(int iStart, int iFinish)
         {
-            return Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.AWA, iStart, iFinish);
+            return AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.AWA, iStart, iFinish);
         }
 
         public bool UseSOG = false;
@@ -299,7 +309,7 @@ namespace NMEAViewer
             iLastSample = Math.Min(m_Data.GetDataCount() - 1, iLastSample);
 
             rOutData.SetValue(TackAnalysisData.eTackDataTypes.InitialHeading,
-                                Util.ContainAngle0To360(m_Data.GetDataAtIndex(iFirstSample, NMEACruncher.DataTypes.BoatHeading))
+                                AngleUtil.ContainAngle0To360(m_Data.GetDataAtIndex(iFirstSample, NMEACruncher.DataTypes.BoatHeading))
                     );
             rOutData.SetValue(TackAnalysisData.eTackDataTypes.WindDirectionAtStart,
                                 TWD(iFirstSample)
@@ -335,7 +345,7 @@ namespace NMEAViewer
 
             //Look at the curve, when do we start changing to the iSlowest / tack point. When do we stop changing direction consistently
             int iStartOfTurn = iThroughWind - 1;
-            double fDelta = Util.ShortAngle(m_Data.GetDataAtIndex(iStartOfTurn, NMEACruncher.DataTypes.AWA), m_Data.GetDataAtIndex(iThroughWind, NMEACruncher.DataTypes.AWA));
+            double fDelta = AngleUtil.ShortAngle(m_Data.GetDataAtIndex(iStartOfTurn, NMEACruncher.DataTypes.AWA), m_Data.GetDataAtIndex(iThroughWind, NMEACruncher.DataTypes.AWA));
             //Look at steering trend over several samples to account for instrument and steering vagaries.
             const int kiRangeToSampleDelta = 8;
             while (iStartOfTurn > iFirstSample)	//? Could use zero and do the sample range automatically eventually
@@ -344,7 +354,7 @@ namespace NMEAViewer
                 int iEndOfSampleTwo = Math.Min(m_Data.GetDataCount(), iStartOfTurn + kiRangeToSampleDelta);
                 double fEarlierAWAAv = AvAWAInRange(iStartOfSampleOne, iStartOfTurn);
                 double fLaterAWAAv = AvAWAInRange(iStartOfTurn, iEndOfSampleTwo);
-                double fPriorDelta = Util.ShortAngle(fEarlierAWAAv, fLaterAWAAv);
+                double fPriorDelta = AngleUtil.ShortAngle(fEarlierAWAAv, fLaterAWAAv);
                 if ((fPriorDelta > 0.0f) ^ (fDelta > 0.0f))
                 {
                     //Counter steering! Perhaps go further though?
@@ -357,14 +367,14 @@ namespace NMEAViewer
             int iEndOfTurn = iThroughWind + 1;
             int kMaxSearchForEnd = Math.Min(iLastSample + 10, m_Data.GetDataCount()); //? Could use zero and do the sample range automatically
             const int kSamplesToCheckForChange = 4;
-            fDelta = Util.ShortAngle(m_Data.GetDataAtIndex(iStartOfTurn, NMEACruncher.DataTypes.AWA), m_Data.GetDataAtIndex(iThroughWind, NMEACruncher.DataTypes.AWA));
+            fDelta = AngleUtil.ShortAngle(m_Data.GetDataAtIndex(iStartOfTurn, NMEACruncher.DataTypes.AWA), m_Data.GetDataAtIndex(iThroughWind, NMEACruncher.DataTypes.AWA));
             while (iEndOfTurn < kMaxSearchForEnd)
             {
                 int iStartOfSampleOne = Math.Max(0, iEndOfTurn - kSamplesToCheckForChange);
                 int iEndOfSampleTwo = Math.Min(m_Data.GetDataCount(), iEndOfTurn + kSamplesToCheckForChange);
                 double fEarlierAWAAv = AvAWAInRange(iStartOfSampleOne, iEndOfTurn);
                 double fLaterAWAAv = AvAWAInRange(iEndOfTurn, iEndOfSampleTwo);
-                double fPriorDelta = Util.ShortAngle(fEarlierAWAAv, fLaterAWAAv);
+                double fPriorDelta = AngleUtil.ShortAngle(fEarlierAWAAv, fLaterAWAAv);
                 if ((fPriorDelta > 0.0f) ^ (fDelta > 0.0f))
                 {
                     //Counter steering!....
@@ -379,7 +389,7 @@ namespace NMEAViewer
                         fEarlierAWAAv = AvAWAInRange(iStartOfSampleOne, iEndOfTurn);
                         fLaterAWAAv = AvAWAInRange(iEndOfTurn, iEndOfSampleTwo);
 
-                        double fNewPriorDelta = Util.ShortAngle(fEarlierAWAAv, fLaterAWAAv);
+                        double fNewPriorDelta = AngleUtil.ShortAngle(fEarlierAWAAv, fLaterAWAAv);
                         if ((fDelta > 0.0f) == (fNewPriorDelta > 0.0f))
                         {
                             //End of counter steering!
@@ -398,14 +408,14 @@ namespace NMEAViewer
             {
                 const int kiSamplesToScan = 10;
                 int iPreAngleStart = Math.Max(0, iStartOfTurn - kiSamplesToScan);
-                double fAWAEntry = Math.Abs(Util.ContainAngleMinus180To180(AvAWAInRange(iPreAngleStart, iStartOfTurn)));
+                double fAWAEntry = Math.Abs(AngleUtil.ContainAngleMinus180To180(AvAWAInRange(iPreAngleStart, iStartOfTurn)));
                 if ((fAWAEntry > fAWAMin) && (fAWAEntry < fAWAMax))	//Are we outside the upwind and downwind cones?
                 {
                     return null;
                 }
 
                 int iEndOfPostTurn = Math.Min(iEndOfTurn + kiSamplesToScan, m_Data.GetDataCount());
-                double fAWAExit = Math.Abs(Util.ContainAngleMinus180To180(AvAWAInRange(iEndOfTurn, iEndOfPostTurn)));
+                double fAWAExit = Math.Abs(AngleUtil.ContainAngleMinus180To180(AvAWAInRange(iEndOfTurn, iEndOfPostTurn)));
                 if ((fAWAExit > fAWAMin) && (fAWAExit < fAWAMax))	//Are we outside the upwind and downwind cones?
                 {
                     return null;
@@ -413,8 +423,8 @@ namespace NMEAViewer
             }
 
             //Store wind information
-            rOutData.SetValue(TackAnalysisData.eTackDataTypes.AverageTWD, Util.ContainAngle0To360(Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.TWD, iStartOfTurn, iEndOfTurn)));
-            rOutData.SetValue(TackAnalysisData.eTackDataTypes.AverageTWS, Util.CalculateAverage(m_Data, NMEACruncher.DataTypes.TWS, iStartOfTurn, iEndOfTurn));
+            rOutData.SetValue(TackAnalysisData.eTackDataTypes.AverageTWD, AngleUtil.ContainAngle0To360(AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.TWD, iStartOfTurn, iEndOfTurn)));
+            rOutData.SetValue(TackAnalysisData.eTackDataTypes.AverageTWS, AngleUtil.CalculateAverage(m_Data, NMEACruncher.DataTypes.TWS, iStartOfTurn, iEndOfTurn));
 
             int iSlowPoint = 0;
             double fSlowestSpdPostTack = 666.6f;
@@ -489,14 +499,14 @@ namespace NMEAViewer
 
                 //TODO: Work out discrepency here - downwind seems dodgy
                 //Calculate entry and exit directions, and tacking angle
-                double fEntryDirection = Util.ContainAngle0To360(Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.GPSHeading, iFirstSample, iEntryEnd));
-                double fExitDirection = Util.ContainAngle0To360(Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.GPSHeading, iExitStart, iLastSample));
-                double fTackingAngle = Util.ShortAngle(fEntryDirection, fExitDirection);
+                double fEntryDirection = AngleUtil.ContainAngle0To360(AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.GPSHeading, iFirstSample, iEntryEnd));
+                double fExitDirection = AngleUtil.ContainAngle0To360(AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.GPSHeading, iExitStart, iLastSample));
+                double fTackingAngle = AngleUtil.ShortAngle(fEntryDirection, fExitDirection);
                 rOutData.SetValue(TackAnalysisData.eTackDataTypes.AngleTackedThrough, fTackingAngle);
 
-                double fEntryDirection_Steered = Util.ContainAngle0To360(Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.BoatHeading, iFirstSample, iEntryEnd));
-                double fExitDirection_Steered = Util.ContainAngle0To360(Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.BoatHeading, iExitStart, iLastSample));
-                double fTackingAngle_Steered = Util.ShortAngle(fEntryDirection_Steered, fExitDirection_Steered);
+                double fEntryDirection_Steered = AngleUtil.ContainAngle0To360(AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.BoatHeading, iFirstSample, iEntryEnd));
+                double fExitDirection_Steered = AngleUtil.ContainAngle0To360(AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.BoatHeading, iExitStart, iLastSample));
+                double fTackingAngle_Steered = AngleUtil.ShortAngle(fEntryDirection_Steered, fExitDirection_Steered);
 
                 rOutData.SetValue(TackAnalysisData.eTackDataTypes.AngleSteeredThrough, fTackingAngle_Steered);
                 //That was lovely. How, about, leeway.
@@ -534,13 +544,13 @@ namespace NMEAViewer
                 //	-	Going deep to accelerate
                 //	-	1) Get final heading
                 int iEndOfFinalHeading = Math.Min(iBackToNinetyPercent + (iInitialSpdNum * 2), m_Data.GetDataCount());
-                double fFinalHeading = Util.ContainAngle0To360(Util.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.GPSHeading, iBackToNinetyPercent, iEndOfFinalHeading));
+                double fFinalHeading = AngleUtil.ContainAngle0To360(AngleUtil.CalculateAngleAverage(m_Data, NMEACruncher.DataTypes.GPSHeading, iBackToNinetyPercent, iEndOfFinalHeading));
 
                 //	-	2) Calculate amount of slipping below this
                 double fDistanceSailedBelowFinalHeading = 0.0f;
                 for (int i = iThroughWind; i < iEndOfFinalHeading; i++)
                 {
-                    double fHeadingDelta = Util.ShortAngle(GPSHeading(i), fFinalHeading);
+                    double fHeadingDelta = AngleUtil.ShortAngle(GPSHeading(i), fFinalHeading);
                     if ((fHeadingDelta > 0.0f) ^ (fTackingAngle > 0.0f))
                     {
                         double fDistance = BoatSpeed(i) * (SampleTime(i) - SampleTime(i - 1)) * (1.0f / 3600.0f);
