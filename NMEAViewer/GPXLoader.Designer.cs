14b55f483454d94a4c2c51ccb9d4d48b41691f53
Tom <theonecalledtom@gmail.com>
2021-11-27 11:19:15 -0800

    Added GPX loading library, added some example code

--- NMEAViewer/GPXLoader.Designer.cs
+++ NMEAViewer/GPXLoader.Designer.cs
@@ -31,22 +31,23 @@ namespace NMEAViewer
         {
             this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.gMapComponent1 = new NMEAViewer.GMapComponent(this.components);
-            this.LoadGPX = new System.Windows.Forms.Button();
-            this.PathOffset = new System.Windows.Forms.TrackBar();
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.RemoveTWD = new System.Windows.Forms.Button();
+            this.AddTWD = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.TWD = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.AddTWD = new System.Windows.Forms.Button();
-            this.RemoveTWD = new System.Windows.Forms.Button();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.LoadGPX = new System.Windows.Forms.Button();
+            this.PathOffset = new System.Windows.Forms.TrackBar();
+            this.gMapComponent1 = new NMEAViewer.GMapComponent(this.components);
+            this.openGPXDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.PathOffset)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PathOffset)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -72,67 +73,23 @@ namespace NMEAViewer
             this.splitContainer1.SplitterDistance = 465;
             this.splitContainer1.TabIndex = 0;
             // 
-            // gMapComponent1
+            // RemoveTWD
             // 
-            this.gMapComponent1.Bearing = 0F;
-            this.gMapComponent1.CanDragMap = true;
-            this.gMapComponent1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gMapComponent1.EmptyTileColor = System.Drawing.Color.Navy;
-            this.gMapComponent1.GrayScaleMode = false;
-            this.gMapComponent1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
-            this.gMapComponent1.LevelsKeepInMemory = 5;
-            this.gMapComponent1.Location = new System.Drawing.Point(0, 0);
-            this.gMapComponent1.MarkersEnabled = true;
-            this.gMapComponent1.MaxZoom = 2;
-            this.gMapComponent1.MinZoom = 2;
-            this.gMapComponent1.MouseWheelZoomEnabled = true;
-            this.gMapComponent1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
-            this.gMapComponent1.Name = "gMapComponent1";
-            this.gMapComponent1.NegativeMode = false;
-            this.gMapComponent1.PolygonsEnabled = true;
-            this.gMapComponent1.RetryLoadTile = 0;
-            this.gMapComponent1.RoutesEnabled = true;
-            this.gMapComponent1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
-            this.gMapComponent1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
-            this.gMapComponent1.ShowTileGridLines = false;
-            this.gMapComponent1.Size = new System.Drawing.Size(931, 705);
-            this.gMapComponent1.TabIndex = 0;
-            this.gMapComponent1.Zoom = 0D;
+            this.RemoveTWD.Location = new System.Drawing.Point(37, 192);
+            this.RemoveTWD.Name = "RemoveTWD";
+            this.RemoveTWD.Size = new System.Drawing.Size(212, 51);
+            this.RemoveTWD.TabIndex = 5;
+            this.RemoveTWD.Text = "Remove TWD";
+            this.RemoveTWD.UseVisualStyleBackColor = true;
             // 
-            // LoadGPX
+            // AddTWD
             // 
-            this.LoadGPX.Location = new System.Drawing.Point(37, 23);
-            this.LoadGPX.Name = "LoadGPX";
-            this.LoadGPX.Size = new System.Drawing.Size(212, 51);
-            this.LoadGPX.TabIndex = 0;
-            this.LoadGPX.Text = "Load GPX";
-            this.LoadGPX.UseVisualStyleBackColor = true;
-            // 
-            // PathOffset
-            // 
-            this.PathOffset.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.PathOffset.Location = new System.Drawing.Point(0, 615);
-            this.PathOffset.Maximum = 100;
-            this.PathOffset.Name = "PathOffset";
-            this.PathOffset.Size = new System.Drawing.Size(931, 90);
-            this.PathOffset.TabIndex = 1;
-            this.PathOffset.Scroll += new System.EventHandler(this.PathOffset_Scroll);
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(0, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 705);
-            this.splitter1.TabIndex = 1;
-            this.splitter1.TabStop = false;
-            // 
-            // splitter2
-            // 
-            this.splitter2.Location = new System.Drawing.Point(3, 0);
-            this.splitter2.Name = "splitter2";
-            this.splitter2.Size = new System.Drawing.Size(3, 705);
-            this.splitter2.TabIndex = 2;
-            this.splitter2.TabStop = false;
+            this.AddTWD.Location = new System.Drawing.Point(37, 105);
+            this.AddTWD.Name = "AddTWD";
+            this.AddTWD.Size = new System.Drawing.Size(212, 51);
+            this.AddTWD.TabIndex = 4;
+            this.AddTWD.Text = "Add TWD";
+            this.AddTWD.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
@@ -162,23 +119,67 @@ namespace NMEAViewer
             this.Time.Name = "Time";
             this.Time.Width = 200;
             // 
-            // AddTWD
+            // splitter2
             // 
-            this.AddTWD.Location = new System.Drawing.Point(37, 105);
-            this.AddTWD.Name = "AddTWD";
-            this.AddTWD.Size = new System.Drawing.Size(212, 51);
-            this.AddTWD.TabIndex = 4;
-            this.AddTWD.Text = "Add TWD";
-            this.AddTWD.UseVisualStyleBackColor = true;
+            this.splitter2.Location = new System.Drawing.Point(3, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(3, 705);
+            this.splitter2.TabIndex = 2;
+            this.splitter2.TabStop = false;
             // 
-            // RemoveTWD
+            // splitter1
             // 
-            this.RemoveTWD.Location = new System.Drawing.Point(37, 192);
-            this.RemoveTWD.Name = "RemoveTWD";
-            this.RemoveTWD.Size = new System.Drawing.Size(212, 51);
-            this.RemoveTWD.TabIndex = 5;
-            this.RemoveTWD.Text = "Remove TWD";
-            this.RemoveTWD.UseVisualStyleBackColor = true;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 705);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // LoadGPX
+            // 
+            this.LoadGPX.Location = new System.Drawing.Point(37, 23);
+            this.LoadGPX.Name = "LoadGPX";
+            this.LoadGPX.Size = new System.Drawing.Size(212, 51);
+            this.LoadGPX.TabIndex = 0;
+            this.LoadGPX.Text = "Load GPX";
+            this.LoadGPX.UseVisualStyleBackColor = true;
+            // 
+            // PathOffset
+            // 
+            this.PathOffset.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PathOffset.Location = new System.Drawing.Point(0, 615);
+            this.PathOffset.Maximum = 100;
+            this.PathOffset.Name = "PathOffset";
+            this.PathOffset.Size = new System.Drawing.Size(931, 90);
+            this.PathOffset.TabIndex = 1;
+            this.PathOffset.Scroll += new System.EventHandler(this.PathOffset_Scroll);
+            // 
+            // gMapComponent1
+            // 
+            this.gMapComponent1.Bearing = 0F;
+            this.gMapComponent1.CanDragMap = true;
+            this.gMapComponent1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gMapComponent1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapComponent1.GrayScaleMode = false;
+            this.gMapComponent1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapComponent1.LevelsKeepInMemory = 5;
+            this.gMapComponent1.Location = new System.Drawing.Point(0, 0);
+            this.gMapComponent1.MarkersEnabled = true;
+            this.gMapComponent1.MaxZoom = 2;
+            this.gMapComponent1.MinZoom = 2;
+            this.gMapComponent1.MouseWheelZoomEnabled = true;
+            this.gMapComponent1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapComponent1.Name = "gMapComponent1";
+            this.gMapComponent1.NegativeMode = false;
+            this.gMapComponent1.PolygonsEnabled = true;
+            this.gMapComponent1.RetryLoadTile = 0;
+            this.gMapComponent1.RoutesEnabled = true;
+            this.gMapComponent1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapComponent1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapComponent1.ShowTileGridLines = false;
+            this.gMapComponent1.Size = new System.Drawing.Size(931, 705);
+            this.gMapComponent1.TabIndex = 0;
+            this.gMapComponent1.Zoom = 0D;
             // 
             // GPXLoader
             // 
@@ -194,8 +195,8 @@ namespace NMEAViewer
             this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.PathOffset)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PathOffset)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -213,5 +214,6 @@ namespace NMEAViewer
         private System.Windows.Forms.DataGridViewTextBoxColumn Time;
         private System.Windows.Forms.Splitter splitter2;
         private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.OpenFileDialog openGPXDialog;
     }
 }
--- NMEAViewer/GPXLoader.cs
+++ NMEAViewer/GPXLoader.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Geo.Gps;
 using GMap.NET.WindowsForms;
 
 namespace NMEAViewer
@@ -48,12 +50,42 @@ namespace NMEAViewer
 
         private void GPXLoader_Load(object sender, EventArgs e)
         {
+            //Pick a file
+            openGPXDialog.Title = "Open GPX file";
+            if (!string.IsNullOrEmpty(m_GPXFileName))
+            {
+                openGPXDialog.InitialDirectory = System.IO.Path.GetDirectoryName(m_GPXFileName);
+                openGPXDialog.FileName = m_GPXFileName;
+            }
+            if (openGPXDialog.ShowDialog() == DialogResult.OK)
+            {
+                m_GPXFileName = openGPXDialog.FileName;
+                using (var stream = new FileStream(m_GPXFileName, FileMode.Open))
+                {
+                    var data = GpsData.Parse(stream);
+                    for (int itrack = 0; itrack < data.Tracks.Count; itrack++)
+                    {
+                        var track = data.Tracks[itrack];
 
+                        for (int isegment = 0; isegment < track.Segments.Count; isegment++)
+                        {
+                            var segment = track.Segments[isegment];
+                            for (int iwaypoint = 0; iwaypoint < segment.Waypoints.Count; iwaypoint++)
+                            {
+                                var waypoint = segment.Waypoints[iwaypoint];
+
+                                //now what :)
+                            }
+                        }
+                    }
+                    //Assert.That(data, Is.EqualTo(null));
+                }
+            }
         }
 
         private void PathOffset_Scroll(object sender, EventArgs e)
         {
-
+            //We're scrubbing the timeline back and forth
         }
     }
 }
